faf41f408e80991e29452ebc8afd26f1321b00b0
Steven <steven@digitalarmada.com.au>
2020-06-13 12:35:53 +1000

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+// eslint-disable-next-line no-unused-vars
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
+
+
+
+
 }
 
 export default App;
